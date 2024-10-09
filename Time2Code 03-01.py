c5c4e4ea786adcc252f230900ec32091eec89e69
ceejaydev <P69828@lutonsfc.ac.uk>
2024-10-09 15:14:39 +0100

    Create Time2Code 03-01.py

---
+++ Time2Code 03-01.py
@@ -0,0 +1,31 @@
+# Capital city program
+
+# -------------------------
+# Subprograms
+# -------------------------
+def check_answer(country, city):
+    if country == "England" and city == "London":
+        return True
+    if country == "Spain" and city == "Madrid":
+        return True
+    else:
+        return False
+
+
+def capital_cities():
+    correct = False
+    while not correct:
+        city = input("What is the capital city of England? : ")
+        correct = check_answer("England", city)
+    
+    correct = False    
+    while not correct:
+        city = input("What is the capital city of Spain? : ")
+        correct = check_answer("Spain", city)
+
+
+# -------------------------
+# Main program
+# -------------------------
+capital_cities()
+print("The quiz is complete.")
